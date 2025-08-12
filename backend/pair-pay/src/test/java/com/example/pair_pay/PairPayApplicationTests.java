c6a4fd8a60d44338197a9c58ca8cfff2a47ae4ac
Keita Kito <kito.keita@fujitsu.com>
2025-08-12 20:56:55 +0900

    backend環境構築

---
+++ backend/pair-pay/src/test/java/com/example/pair_pay/PairPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pair_pay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PairPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
