e0b23dcfc9cc1ab7634a0253d9d5e2ae1132e237
Keita Kito <kito.keita@fujitsu.com>
2025-08-14 10:26:57 +0900

    test

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import PaymentForm from './PaymentForm'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <PaymentForm />
+      </header>
+    </div>
+  );
+}
+
+export default App;
