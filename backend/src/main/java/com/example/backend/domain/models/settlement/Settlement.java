f1123bb47fac75a67658ed6ee1969207ee416cdd
Keita Kito <kito.keita@fujitsu.com>
2025-10-04 13:28:09 +0900

    バックエンドのユーザーに集中したい

--- backend/src/main/java/com/example/backend/domain/models/settlement/Settlement.java
+++
@@ -1,5 +0,0 @@
-package com.example.backend.domain.models.settlement;
-
-public class Settlement {
-    
-}
--- backend/src/main/java/com/example/backend/domain/repository/SettlementRepository.java
+++
@@ -1,5 +0,0 @@
-package com.example.backend.domain.repository;
-
-public class SettlementRepository {
-    
-}
